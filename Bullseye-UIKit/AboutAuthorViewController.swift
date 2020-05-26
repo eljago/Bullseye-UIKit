7ec0589c8987975a74d8aefc70bf739f9bc35be4
Arturo Espinoza Carrasco <clonjago@gmail.com>
2020-05-26 16:51:40 -0400

    Created AboutAuthorViewController

---
+++ Bullseye-UIKit/AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  Bullseye-UIKit
+//
+//  Created by Arturo Espinoza Carrasco on 26-05-20.
+//  Copyright © 2020 Arturo Espinoza Carrasco. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+}
