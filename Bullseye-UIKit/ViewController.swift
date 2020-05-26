52cea978115f02a321be0ba267e93312bf7a9e54
Arturo Espinoza Carrasco <clonjago@gmail.com>
2020-05-26 16:00:41 -0400

    Basic gameplay functionality

--- Bullseye-UIKit/ViewController.swift
+++ Bullseye-UIKit/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var currentValue: Int = 0
+  var targetValue: Int = 0
+  
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var targetLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    startNewRound()
   }
 
   @IBAction func showAlert() {
-    let alert = UIAlertController(title: "Hello World!", message: "This is my first App!", preferredStyle: .alert)
-    let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+    
+    let difference = abs(currentValue - targetValue)
+    let points = 100 - difference
+    let message = "You scored \(points) points"
+    let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
+    
+    startNewRound()
+  }
+  
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    updateCurrentValue()
+  }
+  
+  func updateCurrentValue() {
+    let roundedValue = (slider.value * 100).rounded()
+    currentValue = Int(roundedValue)
+  }
+  
+  func startNewRound() {
+    targetValue = Int.random(in: 1...100)
+    currentValue = 50
+    slider.value = Float(currentValue) / 100.0
+    updateLabels()
+  }
+  
+  func updateLabels() {
+    targetLabel.text = String(targetValue)
   }
 }
 
